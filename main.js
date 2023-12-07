5218861145c228cdeb9eca68ad31d9b5d9d2d220
Rimjhim Gupta <122445673+rimjhimgupta08@users.noreply.github.com>
2023-12-07 13:15:04 +0530

    add all files

---
+++ main.js
@@ -0,0 +1,8 @@
+function playSound(audioName){
+      let audio = new Audio(audioName);
+      audio.play();
+      
+}
+playSound("Mere-Liye-Tum-Kaafi-Ho-Whatsapp-Status---Latest-Whatsapp-Status-2020---Ayushman-Khurana--.mp4");
+
+
